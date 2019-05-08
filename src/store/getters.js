fc7bd93877303f751c36aca736bc4475a22b17d3
zhangchao <529743595@qq.com>
2019-05-08 09:48:43 +0800

    init

--- src/store/getters.js
+++
@@ -1,5 +0,0 @@
-const getters = {
-  info: state => state.app.info
-}
-
-export default getters
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -2,20 +2,15 @@
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
     <HelloWorld msg="Welcome to Your Vue.js App"/>
-    {{info}}
   </div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
-import { mapGetters } from 'vuex'
 
 export default {
   name: 'home',
-  computed: {
-    ...mapGetters(['info'])
-  },
   components: {
     HelloWorld
   }
