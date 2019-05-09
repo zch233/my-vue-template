caf08b94c7501765ba534935a1093ab9c6f43ed8
zhangchao <529743595@qq.com>
2019-05-09 08:44:13 +0800

    增加自定义指令v-loading

--- src/main.js
+++ src/main.js
@@ -6,6 +6,7 @@ import store from './store'
 import 'normalize.css/normalize.css'
 import '@/style/index.scss' // 全局样式
 import '@/permission' // 全局路由控制
+import '@/directive'
 
 Vue.config.productionTip = false
 
