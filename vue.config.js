c5e45bffdda817341c929639574c62fd19af785b
zhangchao <529743595@qq.com>
2019-05-10 11:00:18 +0800

    修复name undefined

--- vue.config.js
+++ vue.config.js
@@ -10,7 +10,7 @@ module.exports = {
   configureWebpack: {
     // provide the app's title in webpack's name field, so that
     // it can be accessed in index.html to inject the correct title.
-    name: name,
+    name: 'my-app',
     resolve: {
       alias: {
         '@': path.join(__dirname, 'src')
