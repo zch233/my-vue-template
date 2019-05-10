369b4e786c5fbe6ea16cd1795d3ba17bcf21acac
zhangchao <529743595@qq.com>
2019-05-10 10:58:47 +0800

    增加webpack配置

--- vue.config.js
+++ vue.config.js
@@ -1,6 +1,20 @@
+'use strict'
+const path = require('path')
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
   outputDir: 'dist',
   assetsDir: 'static',
-  productionSourceMap: false
+  productionSourceMap: false,
+  lintOnSave: process.env.NODE_ENV === 'development',
+  configureWebpack: {
+    // provide the app's title in webpack's name field, so that
+    // it can be accessed in index.html to inject the correct title.
+    name: name,
+    resolve: {
+      alias: {
+        '@': path.join(__dirname, 'src')
+      }
+    }
+  }
 }
