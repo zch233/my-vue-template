ccd3165a57ed245b78f85b6440d47d7bca11f337
zhangchao <529743595@qq.com>
2019-05-14 11:35:59 +0800

    删除懒加载模块，和配置cdn模块

--- vue.config.js
+++ vue.config.js
@@ -1,20 +1,45 @@
 'use strict'
 const path = require('path')
 
+const cdn = {
+  css: [],
+  js: ['http://g.tbcdn.cn/mtb/lib-flexible/0.3.4/??flexible_css.js,flexible.js']
+}
+
+function resolve (dir) {
+  return path.join(__dirname, dir)
+}
+
 module.exports = {
   publicPath: process.env.NODE_ENV === 'production' ? './' : '/',
   outputDir: 'dist',
   assetsDir: 'static',
   productionSourceMap: false,
   lintOnSave: process.env.NODE_ENV === 'development',
+  devServer: {
+    port: 2333,
+    open: true,
+    overlay: {
+      warnings: false,
+      errors: true
+    }
+  },
   configureWebpack: {
-    // provide the app's title in webpack's name field, so that
-    // it can be accessed in index.html to inject the correct title.
-    name: 'my-app',
+    // public/index.html 里的 `title` 会使用这里的 `name`
+    name: 'app',
     resolve: {
       alias: {
-        '@': path.join(__dirname, 'src')
+        '@': resolve('src')
       }
     }
+  },
+  chainWebpack (config) {
+    config.plugin('html')
+      .tap(args => {
+        args[0].cdn = cdn
+        return args
+      })
+    config.plugins.delete('preload') // 删除懒加载模块的 prefetch preload，降低带宽压力
+    config.plugins.delete('prefetch') // 删除懒加载模块的 prefetch preload，降低带宽压力
   }
 }
